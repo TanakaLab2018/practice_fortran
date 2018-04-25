8d2543c592cfb71cf488a2adbc5459b79ff1fb15
B4 <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-04-25 11:58:05 +0900

    teisei time

--- 5/time_bry.f90
+++ 5/time_bry.f90
@@ -1,6 +1,6 @@
 program index
   implicit none
-  integer i,j
+  integer i,j,k
   real a(2,2),b(2,2),c(2,2)
   write(*,*) "index A"
   read(*,*) a(1,1),a(2,1),a(1,2),a(2,2)
@@ -8,7 +8,10 @@ program index
   read(*,*) b(1,1),b(2,1),b(1,2),b(2,2)
   do i=1,2
     do j=1,2
-      c(j,i)=a(j,1)*b(1,i)+a(j,2)*b(2,i)
+      c(j,i)=0
+      do k=1,2
+        c(j,i)=c(j,i)+a(j,k)*b(k,i)
+      end do
     end do
   end do
   write(*,*) c
