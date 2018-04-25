81c836eebee9cdf494c0c7f3d6a1b280231db816
KS <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-04-25 15:20:57 +0900

    二次関数の台型積分

--- 6/nijisekibun_k.f90
+++ 6/nijisekibun_k.f90
@@ -1,18 +1,26 @@
 program nijisekibun
  implicit none
- integer i
- real a,b,c,x,j
+ integer i, N
+ real a,b,c,x,j,f,h
  real L1,L2,S1,S
   print *,"系数？"
    read *, a,b,c
   print *, a, "x^2+", b, "x+", c, "=0"
-  S=0
-   do i=0, 100
-     j=float(i)/100.0
-     L1=a*j**2.0+b*j+c
-     L2=a*(j+0.01)**2.0+b*(j+0.01)+c
-     S1=((L1)+(L2))*(0.010)/2
+  S=0.0
+  print *, "刻み数"
+  read *, N
+  h=1.0/float(N)
+   do i=0, N
+     j=float(i)*h
+     L1=f(j, a, b, c)
+     L2=f(j+h, a, b, c)
+     S1=((L1)+(L2))*(h)/2
      S=S+S1
    end do
   print *, "積分値は", S
 end program nijisekibun
+
+function f(x,a,b,c)
+  real x, a, b, c, f
+  f = a*x**2+b*x+c
+end function
