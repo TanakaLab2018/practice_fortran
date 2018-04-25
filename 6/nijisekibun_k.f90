6d9d59262390361c92a4d6b68c1abab0a3ba02af
KS <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-04-25 15:00:13 +0900

    4/25

--- 6/nijisekibun_k.f90
+++ 6/nijisekibun_k.f90
@@ -7,11 +7,11 @@ program nijisekibun
    read *, a,b,c
   print *, a, "x^2+", b, "x+", c, "=0"
   S=0
-   do i=0, 10
-     j=i/10
+   do i=0, 100
+     j=float(i)/100.0
      L1=a*j**2.0+b*j+c
-     L2=a*(j+0.1)**2.0+b*(j+0.1)+c
-     S1=((L1)+(L2))*(0.10)/2
+     L2=a*(j+0.01)**2.0+b*(j+0.01)+c
+     S1=((L1)+(L2))*(0.010)/2
      S=S+S1
    end do
   print *, "積分値は", S
