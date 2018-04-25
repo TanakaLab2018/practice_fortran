7aae7d06efc96fcaccd9b1cfcfd965d0bbfde888
B4 <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-04-25 16:12:19 +0900

    add

--- 5/arc_bry.f90
+++ 5/arc_bry.f90
@@ -3,7 +3,11 @@ program index
   integer i,j
   real a(2,2),c(2,2)
   write(*,*) "index A"
-  read(*,*) a(1,1),a(2,1),a(1,2),a(2,2)
+  do i=1,2,1
+    do j=1,2,1
+      read(*,*) a(j,i)
+    end do
+  end do
   if(a(1,1)*a(2,2)-a(2,1)*a(1,2)==0) then
     write(*,*) "A^(-1) is none"
   else
