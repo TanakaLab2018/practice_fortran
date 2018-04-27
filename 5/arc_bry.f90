295ebe107c24bafe3c5cd7054c1ed0ddbd0d383d
B4 <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-04-27 10:27:49 +0900

    correction arc_bry

--- 5/arc_bry.f90
+++ 5/arc_bry.f90
@@ -1,20 +1,21 @@
 program index
   implicit none
   integer i,j
-  real a(2,2),c(2,2)
+  real a(2,2),c(2,2),det
   write(*,*) "index A"
   do i=1,2,1
     do j=1,2,1
       read(*,*) a(j,i)
     end do
   end do
-  if(a(1,1)*a(2,2)-a(2,1)*a(1,2)==0) then
+  det=a(1,1)*a(2,2)-a(2,1)*a(1,2)
+  if(det==0) then
     write(*,*) "A^(-1) is none"
   else
-    c(1,1)=a(2,2)/(a(1,1)*a(2,2)-a(2,1)*a(1,2))
-    c(2,1)=-a(2,1)/(a(1,1)*a(2,2)-a(2,1)*a(1,2))
-    c(1,2)=-a(1,2)/(a(1,1)*a(2,2)-a(2,1)*a(1,2))
-    c(2,2)=a(1,1)/(a(1,1)*a(2,2)-a(2,1)*a(1,2))
+    c(1,1)=a(2,2)/det
+    c(2,1)=-a(2,1)/det
+    c(1,2)=-a(1,2)/det
+    c(2,2)=a(1,1)/det
     write(*,*) c
   end if
 end program index
