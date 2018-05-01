58f74ee9774a9780c24a1a309a6099cdfdcc3589
B4 <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-05-01 11:13:58 +0900

    create arcN

--- 6/arcN_bry.f90
+++ 6/arcN_bry.f90
@@ -14,21 +14,23 @@ program index
     end do
   end do
   c=0
-  do j=1,n,1
-    c(j,j)=1
+  do i=1,n,1
+    c(i,i)=1
   end do
-  do j=1,n,1
-    buf=1/a(j,j)
-    do i=1,n,1
+  do i=1,n,1
+    buf=1/a(i,i)
+    do j=1,n,1
       x=a(j,i)*buf
       y=c(j,i)*buf
+      a(j,i)=x
+      c(j,i)=y
     end do
-    do i=1,n,1
-      if(j.ne.i) then
+    do j=1,n,1
+      if(i.ne.j) then
         buf=a(i,j)
         do k=1,n,1
-          a(i,k)=a(i,k)-a(j,k)*buf
-          c(i,k)=c(i,k)-c(j,k)*buf
+          a(k,j)=a(k,j)-a(k,i)*buf
+          c(k,j)=c(k,j)-c(k,i)*buf
         end do
       end if
     end do
