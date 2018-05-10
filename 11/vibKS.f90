bb99e3b9bb3c6566f3f1106369ad0f61076fa80a
KS <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-05-10 17:50:18 +0900

    重ねて描画

--- 11/vibKS.f90
+++ 11/vibKS.f90
@@ -1,9 +1,11 @@
 program vibKS
   character :: jobz,uplo
-  integer   :: lda,lwork=30,info
-  double precision :: m(8,8)=0.0d0,w(8),work(30)
+  integer,parameter :: n=100
+  integer   :: lda,lwork=(10*n),info
 
-  integer,parameter :: n=8
+  double precision :: m(n,n)=0.0d0,w(n),work(10*n)
+
+
   integer           :: i,j
 
 !微分方程式に現われる行列
@@ -19,12 +21,15 @@ m=-m
 !write(*,*) m
 open(11,file='vibKS.txt',status='replace')
 
-write(*,*) "j=?"
-read *, j
+!write(*,*) "j=?"
+!read *, j
 
 call dsyev('v','u',n,m,n,w,work,lwork,info)
 do i=1,n
-write(11,*)  m(i,j)
+  do j=1,n
+write(11,fmt='(f15.8)',advance='no')  m(i,j)
+end do
+write(11,*)
 end do
 close(11)
  ! print *, m(1,1),m(1,2),m(1,3)
