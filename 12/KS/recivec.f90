44e1e7124d26613e85d96ee4588de245a94caccd
KS <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-05-16 12:19:59 +0900

    逆格子を求める　説明

--- 12/KS/recivec.f90
+++ 12/KS/recivec.f90
@@ -8,6 +8,7 @@ program recivec
  read (*,*) a
  read (*,*) b
  read (*,*) c
+
   V=a(1)*(b(2)*c(3)-b(3)*c(2))+a(2)*(b(3)*c(1)-b(1)*c(3))+a(3)*(b(1)*c(2)-b(2)*c(1))
 ! write (*,*)  V
  d(1)=2*pi/V*(b(2)*c(3)-b(3)*c(2))
@@ -22,7 +23,7 @@ write (*,*)  e
 
 f(1)=2*pi/V*(a(2)*b(3)-a(3)*b(2))
 f(2)=2*pi/V*(a(3)*b(1)-a(1)*b(3))
-f(3)=2*pi/V*(a(1)*b(2)-a(2)*b(1))h
+f(3)=2*pi/V*(a(1)*b(2)-a(2)*b(1))
 write (*,*)  f
 
 end program recivec
