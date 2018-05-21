f23741411a0fe2a9341765d3e2f67b85e4686738
Ryoi_laptop <ryoi084@outlook.jp>
2018-05-21 10:35:02 +0900

    edit

--- 13/KS/bussei_tb.f90
+++ 13/KS/bussei_tb.f90
@@ -1,12 +1,10 @@
 program bussei
-  !数の定義
   implicit none
   integer, parameter :: n=2
   integer, parameter :: lda=n,ldvl=n,ldvr=n,lwork=10*n
   integer info
   double precision A(n*n),vl(n*n),vr(n*n),work(lwork),wr(n),wi(n)
 
-  !行列Aの出力
   write(*,*) "Ax=λx call x,λ"
   a(1)=3.0
   a(2)=-2.0
@@ -16,10 +14,8 @@ program bussei
   write(*,*) a(1),a(3)
   write(*,*) a(2),a(4)
 
-  !固有値計算
   call dgeev('V','N',n,a,lda,wr,wi,vl,ldvl,vr, ldvr, work, lwork, info )
 
-  !固有値、固有ベクトルの出力
   write(*,*) "eigenvalue λ real"
   call force(wr,n,1)
   write(*,*) "eigenvalue λ imagin"
@@ -31,7 +27,6 @@ program bussei
 
 end program bussei
 
-  !固有値の出力の仕方
 subroutine force(x,n,m)
   integer i,j,n,m
   double precision x(n,m)
@@ -42,4 +37,4 @@ subroutine force(x,n,m)
     write(*,*)
   end do
   write(*,*) "==========================="
-end subroutine 
+end subroutine
