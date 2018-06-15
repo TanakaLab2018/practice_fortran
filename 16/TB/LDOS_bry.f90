54ade9db903c959090f2a948fdfbe6f6d606a0d5
TB <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-06-15 13:34:43 +0900

    LDOS 支援求む

---
+++ 16/TB/LDOS_bry.f90
@@ -0,0 +1,57 @@
+program bussei
+  !数の定義
+  implicit none
+  integer, parameter :: n=4*100,m=100
+  !integer, parameter :: lda=n,ldvl=n,ldvr=n,lwork=10*n
+  complex(kind(0d0)), parameter :: ci = (0.0d0,1.0d0)
+  double precision, parameter :: pi=4.0d0*atan(1.0d0)
+  double precision :: mu=3.5d0, Lambda=1.0d0, ky=0.0d0 ,w(n),E,p,t
+  integer :: i,j,ipiv(n),lwork=n*n
+  complex(kind(0d0)) :: delta=1.0d0,work(n*n),A(n,n)
+  integer info
+  !complex(kind(0d0)) :: ,vl(n,n),vr(n,n),work(lwork),wr(n),wi(n)
+
+  !行列Aの出力
+  open(17,file='date1.txt',status='replace')
+  do j=0,600
+    ky=0.0d0
+    t=0.01d0*j
+    E=-3.0d0+t
+  A=0.0d0
+  do i=1,m
+    A(4*(i-1)+1,4*(i-1)+1)=E+ci*0.000010d0-Lambda*(ky*ky+2)-mu
+    A(4*(i-1)+2,4*(i-1)+2)=E+ci*0.000010d0-Lambda*(ky*ky+2)-mu
+    A(4*(i-1)+3,4*(i-1)+3)=E+ci*0.000010d0+Lambda*(ky*ky+2)+mu
+    A(4*(i-1)+4,4*(i-1)+4)=E+ci*0.000010d0+Lambda*(ky*ky+2)+mu
+    A(4*(i-1)+1,4*(i-1)+4)=E+ci*0.000010d0-delta
+    A(4*(i-1)+2,4*(i-1)+3)=E+ci*0.000010d0+delta
+    A(4*(i-1)+3,4*(i-1)+2)=E+ci*0.000010d0+conjg(delta)
+    A(4*(i-1)+4,4*(i-1)+1)=E+ci*0.000010d0-conjg(delta)
+  end do
+  do i=1,m-1
+    A(4*(i-1)+1,4*i+1)=Lambda
+    A(4*(i-1)+2,4*i+2)=Lambda
+    A(4*(i-1)+3,4*i+3)=-Lambda
+    A(4*(i-1)+4,4*i+4)=-Lambda
+
+    A(4*i+1,4*(i-1)+1)=Lambda
+    A(4*i+2,4*(i-1)+2)=Lambda
+    A(4*i+3,4*(i-1)+3)=-Lambda
+    A(4*i+4,4*(i-1)+4)=-Lambda
+  end do
+  !do i=1,n
+  !  A(i,i)=A(i,i)+
+  !end do
+
+  !逆行列計算
+  call zgetrf(n,n,A,n,ipiv,info)
+  call zgetri(n,A,n,ipiv,work,lwork,info)
+  !状態密度の導出
+  p=(aimag(A(1,1)+A(2,2)))/pi
+    write(17,*) E,p
+  end do
+  close(17)
+
+  write(*,*) "example completed."
+
+end program bussei
