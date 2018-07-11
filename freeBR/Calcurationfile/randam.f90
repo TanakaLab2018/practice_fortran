45d2a8081b53e98b095b907d6340e18015d827db
TB <r.ohashi@rover.nuap.nagoya-u.ac.jp>
2018-07-11 12:00:33 +0900

    追加　乱数

---
+++ freeBR/Calcurationfile/randam.f90
@@ -0,0 +1,21 @@
+program randam
+    implicit none
+    integer :: k,clock,nRand,x
+    integer,allocatable :: seed(:)
+    real(4) :: rand(100000)
+    
+    x=0.0d0
+!乱数決める
+    call RANDOM_SEED(size=nRand)
+    ALLOCATE(seed(nRand))
+    call SYSTEM_CLOCK(count=clock)
+    seed=clock
+    call RANDOM_SEED(put=seed)
+    call RANDOM_NUMBER(rand)
+    rand=rand*10
+    x=int(rand(3))
+!乱数表示
+    write(*,*) x
+
+end program randam
+
